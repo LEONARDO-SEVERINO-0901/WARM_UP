99af12d009a54d16101a408ba0e02a93646e2854
Leonardo Severino <91889648+LEONARDO-SEVERINO-0901@users.noreply.github.com>
2021-10-29 18:23:59 -0300

    Add files via upload

--- LEONARDO/identifier/test/TestIdentifier.c
+++ LEONARDO/identifier/test/TestIdentifier.c
@@ -16,11 +16,40 @@ TEST(Identifier, TestID1)
 {
   // All of these should pass
   TEST_ASSERT_EQUAL(0, identifier("abc\n"));
-  TEST_ASSERT_EQUAL(0, identifier("aaaaa\n"));
 }
 
 TEST(Identifier, TestID2)
 {
+  // All of these should pass
+  TEST_ASSERT_EQUAL(0, identifier("aaaaa\n"));
+}
+
+TEST(Identifier, TestID3)
+{
   // This test will fail
   TEST_ASSERT_EQUAL(0, identifier("abcdef\n"));
+}
+
+TEST(Identifier, TestID4)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("12345\n"));
+}
+
+TEST(Identifier, TestID5)
+{
+  // All of these should pass
+  TEST_ASSERT_EQUAL(0, identifier("ABCD\n"));
+}
+
+TEST(Identifier, TestID6)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("\n"));
+}
+
+TEST(Identifier, TestID7)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("abcdefg\n"));
 }
--- LEONARDO/identifier/test/test_runners/TestIdentifier_Runner.c
+++ LEONARDO/identifier/test/test_runners/TestIdentifier_Runner.c
@@ -5,4 +5,9 @@ TEST_GROUP_RUNNER(Identifier)
 {
   RUN_TEST_CASE(Identifier, TestID1);
   RUN_TEST_CASE(Identifier, TestID2);
+  RUN_TEST_CASE(Identifier, TestID3);
+  RUN_TEST_CASE(Identifier, TestID4);
+  RUN_TEST_CASE(Identifier, TestID5);
+  RUN_TEST_CASE(Identifier, TestID6);
+  RUN_TEST_CASE(Identifier, TestID7);
 }
