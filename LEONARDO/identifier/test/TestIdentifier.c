7177af73db3c52903e6cb0cf882d68f934c70e85
Leonardo Severino <91889648+LEONARDO-SEVERINO-0901@users.noreply.github.com>
2021-10-31 10:42:29 -0300

    Add files via upload

--- LEONARDO/identifier/test/TestIdentifier.c
+++ LEONARDO/identifier/test/TestIdentifier.c
@@ -14,42 +14,96 @@ TEST_TEAR_DOWN(Identifier)
 
 TEST(Identifier, TestID1)
 {
-  // All of these should pass
-  TEST_ASSERT_EQUAL(0, identifier("abc\n"));
+  // This test will pass
+  TEST_ASSERT_EQUAL(0, identifier("a\n"));
 }
 
 TEST(Identifier, TestID2)
 {
-  // All of these should pass
-  TEST_ASSERT_EQUAL(0, identifier("aaaaa\n"));
+  // This test will pass
+  TEST_ASSERT_EQUAL(1, identifier("abcdef\n"));
 }
 
 TEST(Identifier, TestID3)
 {
-  // This test will fail
-  TEST_ASSERT_EQUAL(0, identifier("abcdef\n"));
+  // This test will pass
+  TEST_ASSERT_EQUAL(0, identifier("abcde\n"));
 }
 
 TEST(Identifier, TestID4)
 {
-  // This test will fail
-  TEST_ASSERT_EQUAL(0, identifier("12345\n"));
+  // This test will pass
+  TEST_ASSERT_EQUAL(1, identifier("abcdefg\n"));
 }
 
 TEST(Identifier, TestID5)
 {
-  // All of these should pass
+  // This test will pass
   TEST_ASSERT_EQUAL(0, identifier("ABCD\n"));
 }
 
 TEST(Identifier, TestID6)
 {
-  // This test will fail
-  TEST_ASSERT_EQUAL(0, identifier("\n"));
+  // This test will pass
+  TEST_ASSERT_EQUAL(1, identifier("\n"));
 }
 
 TEST(Identifier, TestID7)
 {
+  // This test will pass
+  TEST_ASSERT_EQUAL(1, identifier("1234\n"));
+}
+
+TEST(Identifier, TestID8)
+{
+  // This test will pass
+  TEST_ASSERT_EQUAL(0, identifier("aBcdE\n"));
+}
+
+TEST(Identifier, TestID9)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(1, identifier("a\n"));
+}
+
+TEST(Identifier, TestID10)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("abcdef\n"));
+}
+
+TEST(Identifier, TestID11)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(1, identifier("abcde\n"));
+}
+
+TEST(Identifier, TestID12)
+{
   // This test will fail
   TEST_ASSERT_EQUAL(0, identifier("abcdefg\n"));
+}
+
+TEST(Identifier, TestID13)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(1, identifier("ABCD\n"));
+}
+
+TEST(Identifier, TestID14)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("\n"));
+}
+
+TEST(Identifier, TestID15)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(0, identifier("1234\n"));
+}
+
+TEST(Identifier, TestID16)
+{
+  // This test will fail
+  TEST_ASSERT_EQUAL(1, identifier("aBcdE\n"));
 }
--- LEONARDO/identifier/test/test_runners/TestIdentifier_Runner.c
+++ LEONARDO/identifier/test/test_runners/TestIdentifier_Runner.c
@@ -10,4 +10,13 @@ TEST_GROUP_RUNNER(Identifier)
   RUN_TEST_CASE(Identifier, TestID5);
   RUN_TEST_CASE(Identifier, TestID6);
   RUN_TEST_CASE(Identifier, TestID7);
+  RUN_TEST_CASE(Identifier, TestID8);
+  RUN_TEST_CASE(Identifier, TestID9);
+  RUN_TEST_CASE(Identifier, TestID10);
+  RUN_TEST_CASE(Identifier, TestID11);
+  RUN_TEST_CASE(Identifier, TestID12);
+  RUN_TEST_CASE(Identifier, TestID13);
+  RUN_TEST_CASE(Identifier, TestID14);
+  RUN_TEST_CASE(Identifier, TestID15);
+  RUN_TEST_CASE(Identifier, TestID16);	
 }
